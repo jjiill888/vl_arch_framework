64381c6e3612bed1b375dcd5051b96c183b3ba0b
jjiill888 <2389946405@qq.com>
2025-08-13 17:03:28 -0500

    add return

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,9 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+use tauri_plugin_global_shortcut::ShortcutState;
+
+const LIB_HOME: &str = "http://server.15110y.top:8083";
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,6 +13,39 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_shortcuts(["CTRL+Z", "COMMAND+Z"]) // Ctrl+Z handling on all platforms
+                .unwrap()
+                .with_handler(|app, _shortcut, event| {
+                    if event.state == ShortcutState::Pressed {
+                        let js = format!(
+                            r#"(function() {{
+    const iframe = document.querySelector('iframe');
+    const win = iframe?.contentWindow;
+    if (win) {{
+      if (win.history.length > 1) {{
+        win.history.back();
+      }} else {{
+        win.location.href = "{home}";
+      }}
+    }} else {{
+      if (history.length > 1) {{
+        history.back();
+      }} else {{
+        location.href = "{home}";
+      }}
+    }}
+  }})();"#,
+                            home = LIB_HOME
+                        );
+                        for window in app.webview_windows().values() {
+                            let _ = window.eval(&js);
+                        }
+                    }
+                })
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![greet])
         .on_page_load(|window, _| {
             let script = r#"
